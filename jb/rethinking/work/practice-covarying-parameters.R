92854d0164831f82d177677a871c7d9731579f14
David VandeBunte <davidvandebunte@gmail.com>
2022-03-06 08:40:56 -0600

    jb: Convert medium/hard questions in Chp. 14

--- jb/rethinking/work/practice-covarying-parameters.R
+++
@@ -1,7 +0,0 @@
-source("iplot.R")
-library(rethinking)
-
-source("practice-gaussian-process-oceanic-waic.R")
-source("practice-phylogenetic-distance-reversed.R")
-source("practice-covarying-bangladesh.R")
-source("practice-oxford-boys.R")
