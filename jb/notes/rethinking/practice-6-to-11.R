ff05187fd19def44b21698797f06dbd4fcea2d20
David VandeBunte <davidvandebunte@gmail.com>
2022-03-19 11:32:49 -0500

    practice-6-to-11.R: Remove duplicate source

--- jb/notes/rethinking/practice-6-to-11.R
+++ jb/notes/rethinking/practice-6-to-11.R
@@ -3,4 +3,3 @@ suppressPackageStartupMessages(library(rethinking))
 
 source("practice-deconfound-chp6-models.R")
 source("practice-overfitting-underfitting.R")
-source("practice-model-interactions.R")
